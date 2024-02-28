5ff55707f72764b851eabd3ac3200d9f3579232b
MaxQuin1 <maximoquinteroescobar8@gmail.com>
2024-02-28 12:27:11 -0600

    lo hago con tailwind

--- src/page/modulo4.js
+++ src/page/modulo4.js
@@ -1,90 +1,87 @@
 import React from "react";
 import logo from '../img/UT.png'
 import user from '../img/user.png'
-import './modulo4.css'
-
-//Módulo 4: Control de Alumnos Asesorados:
 
 function Modulo4() {
   return (
     <>
       <header>
-        <nav class="nav">
-          <div class="flex-item logo">
-            <img src={logo} alt="logo-ut" class="header__nav" />
+        <nav className="bg-[#325B87] px-14 py-5 flex flex-wrap justify-between items-center">
+          <div className="flex m-auto">
+            <img src={logo} alt="logo-ut" className="w-52 h-14 filter drop-shadow-lg" />
           </div>
-          <div class="flex-item">
-            <ul>
-              <li class="div__ul-li">
-                <a class="ul__li-a">Gestión de estadias</a>
+          <div className="flex m-auto px-5 mt-2">
+            <ul className="flex flex-wrap">
+              <li className="px-5 py-1">
+                <a className="text-white">Gestión de estadias</a>
               </li>
-              <li class="div__ul-li">
-                <a class="ul__li-a">Proyecto</a>
+              <li className="px-5 py-1">
+                <a className="text-white">Proyecto</a>
               </li>
-              <li class="div__ul-li">
-                <a class="ul__li-a">Calendario</a>
+              <li className="px-5 py-1">
+                <a className="text-white">Calendario</a>
               </li>
-              <li class="div__ul-li">
-                <a class="ul__li-a">Documentos</a>
+              <li className="px-5 py-1">
+                <a className="text-white">Documentos</a>
               </li>
             </ul>
           </div>
-          <div class="flex-item user-img">
-            <img src={user} alt="User"/>
+          <div className="flex m-auto mt-2">
+            <img src={user} alt="User" className="w-16 h-16" />
           </div>
         </nav>
       </header>
-      <div class="saludo">
-      Bienvenido (docente)!
+      <div className="text-zinc-400 border-b border-zinc-400 px-5 py-3 text-xl mx-5">
+        Bienvenido (docente)!
       </div>
-      <div class="titulo">
-        <h1>Control de alumnos asesorados</h1>
+      <div className="titulo">
+        <h1 className="px-10 py-3 text-3xl font-bold">Control de alumnos asesorados</h1>
       </div>
-      <div class="container">
-        <div class="container__fila">
-          <div class="container__items">
-            <h6>Estudiante #</h6>
+      <div className="container mt-10">
+        <div className="flex flex-wrap justify-center">
+          <div className="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 className="text-xl text-center text-blue-600">Estudiante #</h6>
             <p>Proyecto:$proyecto</p>
             <p>Matricula:$matricula</p>
           </div>
-          <div class="container__items">
-            <h6>Estudiante #</h6>
-            <p>Proyecto:$proyecto</p>
-            <p>Matricula:$matricula</p>
-          </div>
-        </div>
-        <div class="container__fila">
-          <div class="container__items">
-            <h6>Estudiante #</h6>
-            <p>Proyecto:$proyecto</p>
-            <p>Matricula:$matricula</p>
-          </div>
-          <div class="container__items">
-            <h6>Estudiante #</h6>
+          <div className="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 className="text-xl text-center text-blue-600">Estudiante #</h6>
             <p>Proyecto:$proyecto</p>
             <p>Matricula:$matricula</p>
           </div>
         </div>
-        <div class="container__fila">
-          <div class="container__items">
-            <h6>Estudiante #</h6>
+        <div className="flex flex-wrap justify-center">
+          <div className="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 className="text-xl text-center text-blue-600">Estudiante #</h6>
             <p>Proyecto:$proyecto</p>
             <p>Matricula:$matricula</p>
           </div>
-          <div class="container__items">
-            <h6>Estudiante #</h6>
+          <div className="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 className="text-xl text-center text-blue-600">Estudiante #</h6>
             <p>Proyecto:$proyecto</p>
             <p>Matricula:$matricula</p>
           </div>
         </div>
-        <div class="container__fila">
-          <div class="container__items">
-            <h6>Estudiante #</h6>
+        <div className="flex flex-wrap justify-center">
+          <div className="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 className="text-xl text-center text-blue-600">Estudiante #</h6>
             <p>Proyecto:$proyecto</p>
             <p>Matricula:$matricula</p>
           </div>
-          <div class="container__items">
-            <h6>Estudiante #</h6>
+          <div className="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 className="text-xl text-center text-blue-600">Estudiante #</h6>
+            <p>Proyecto:$proyecto</p>
+            <p>Matricula:$matricula</p>
+          </div>
+        </div>
+        <div className="flex flex-wrap justify-center">
+          <div className="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 className="text-xl text-center text-blue-600">Estudiante #</h6>
+            <p>Proyecto:$proyecto</p>
+            <p>Matricula:$matricula</p>
+          </div>
+          <div className="container__items border rounded-lg shadow-md p-4 mx-10 my-5 max-w-md">
+            <h6 className="text-xl text-center text-blue-600">Estudiante #</h6>
             <p>Proyecto:$proyecto</p>
             <p>Matricula:$matricula</p>
           </div>
@@ -95,3 +92,100 @@ function Modulo4() {
 }
 
 export default Modulo4;
+
+// import logo from '../img/UT.png'
+// import user from '../img/user.png'
+// import './modulo4.css'
+
+//Módulo 4: Control de Alumnos Asesorados:
+
+// function Modulo4() {
+//   return (
+//     <>
+//       <header>
+//         <nav class="bg-blue-700 p-4">
+//           <div class="flex-item logo">
+//             <img src={logo} alt="logo-ut" class="header__nav" />
+//           </div>
+//           <div class="flex-item">
+//             <ul>
+//               <li class="div__ul-li">
+//                 <a class="ul__li-a">Gestión de estadias</a>
+//               </li>
+//               <li class="div__ul-li">
+//                 <a class="ul__li-a">Proyecto</a>
+//               </li>
+//               <li class="div__ul-li">
+//                 <a class="ul__li-a">Calendario</a>
+//               </li>
+//               <li class="div__ul-li">
+//                 <a class="ul__li-a">Documentos</a>
+//               </li>
+//             </ul>
+//           </div>
+//           <div class="flex-item user-img">
+//             <img src={user} alt="User"/>
+//           </div>
+//         </nav>
+//       </header>
+//       <div class="saludo">
+//       Bienvenido (docente)!
+//       </div>
+//       <div class="titulo">
+//         <h1>Control de alumnos asesorados</h1>
+//       </div>
+//       <div class="container">
+//         <div class="container__fila">
+//           <div class="container__items">
+//             <h6>Estudiante #</h6>
+//             <p>Proyecto:$proyecto</p>
+//             <p>Matricula:$matricula</p>
+//           </div>
+//           <div class="container__items">
+//             <h6>Estudiante #</h6>
+//             <p>Proyecto:$proyecto</p>
+//             <p>Matricula:$matricula</p>
+//           </div>
+//         </div>
+//         <div class="container__fila">
+//           <div class="container__items">
+//             <h6>Estudiante #</h6>
+//             <p>Proyecto:$proyecto</p>
+//             <p>Matricula:$matricula</p>
+//           </div>
+//           <div class="container__items">
+//             <h6>Estudiante #</h6>
+//             <p>Proyecto:$proyecto</p>
+//             <p>Matricula:$matricula</p>
+//           </div>
+//         </div>
+//         <div class="container__fila">
+//           <div class="container__items">
+//             <h6>Estudiante #</h6>
+//             <p>Proyecto:$proyecto</p>
+//             <p>Matricula:$matricula</p>
+//           </div>
+//           <div class="container__items">
+//             <h6>Estudiante #</h6>
+//             <p>Proyecto:$proyecto</p>
+//             <p>Matricula:$matricula</p>
+//           </div>
+//         </div>
+//         <div class="container__fila">
+//           <div class="container__items">
+//             <h6>Estudiante #</h6>
+//             <p>Proyecto:$proyecto</p>
+//             <p>Matricula:$matricula</p>
+//           </div>
+//           <div class="container__items">
+//             <h6>Estudiante #</h6>
+//             <p>Proyecto:$proyecto</p>
+//             <p>Matricula:$matricula</p>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Modulo4;
