74663f151e7493a7de969305c1e39d307c6a1ab6
Karen-Hoil <karenhoil2004@gmail.com>
2024-02-28 14:24:11 -0500

    Tailwind

--- src/components/headerPresi.js
+++ src/components/headerPresi.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../img/UT.png'
 import user from '../img/user.png'
+import 'tailwindcss/tailwind.css';
 
 function HeaderPresi() {
   return (
--- src/page/modulo6.js
+++ src/page/modulo6.js
@@ -1,73 +1,71 @@
 import React from "react";
-import Proyecto from "../img/proyectos.png"
-import Estudiante from "../img/estudiantes.png"
-import Asesores from "../img/asesores.png"
-import Documento from "../img/documentos.png"
+import Proyecto from "../img/proyectos.png";
+import Estudiante from "../img/estudiantes.png";
+import Asesores from "../img/asesores.png";
+import Documento from "../img/documentos.png";
 import HeaderPresi from "../components/headerPresi";
 
-//Módulo 1: Dashboard presidente:
-
-function Modulo6 (){
-    return(
-        <>
-        <HeaderPresi/>
-        <div class="saludo">
-        <h2>Bienvenido (Presidente)!</h2>
+function Modulo6() {
+  return (
+    <>
+      <HeaderPresi />
+      <div className="saludo">
+        <h2 className="text-2xl">Bienvenido (Presidente)!</h2>
       </div>
       <div className="ag-format-container">
-  <div className="ag-courses_box">
-    <div className="ag-courses_item shadow-drop-bl">
-      <a href="#" className="ag-courses-item_link">
-        <div className="ag-courses-item_bg" />
-        <div style={{ display: "flex" }}>
-          <img src={Proyecto} alt="..." style={{ height: 100 }} />
-          <div className="ag-courses-item_title" style={{textDecoration:"none"}}>Proyectos</div>
+        <div className="flex flex-wrap justify-between items-start px-0 md:px-4 py-8">
+          <div className="w-full md:w-1/2 mb-6 md:mb-0">
+            <a
+              href="#"
+              className="block p-4 rounded-xl ag-courses-item shadow hover:bg-blue-300 hover:text-black transition duration-300 relative mr-30 ml-20"
+            >
+              <div className="h-32 mb-4 flex items-center justify-center">
+                <img src={Proyecto} alt="Proyectos" className="h-full" />
+              </div>
+              <div className="text-xl font-bold">Proyectos</div>
+              <div>Proyectos del área de ingeniería</div>
+            </a>
+          </div>
+          <div className="w-full md:w-1/2 mb-6 md:mb-0">
+            <a
+              href="#"
+              className="block p-4 rounded-xl ag-courses-item shadow hover:bg-blue-300 hover:text-black transition duration-300 relative ml-10"
+            >
+              <div className="h-32 mb-4 flex items-center justify-center">
+                <img src={Asesores} alt="Documentos" className="h-full" />
+              </div>
+              <div className="text-xl font-bold">Asesores</div>
+              <div>Asesores del área de ingeniería</div>
+            </a>
+          </div>
+          <div className="w-full md:w-1/2 mb-6 md:mb-0">
+            <a
+              href="#"
+              className="block p-4 rounded-xl ag-courses-item shadow hover:bg-blue-300 hover:text-black transition duration-300 relative mr-30 ml-10"
+            >
+              <div className="h-32 mb-4 flex items-center justify-center">
+                <img src={Estudiante} alt="Asesores" className="h-full" />
+              </div>
+              <div className="text-xl font-bold">Estudiantes</div>
+              <div>Estudiantes del área de ingeniería</div>
+            </a>
+          </div>
+          <div className="w-full md:w-1/2 mb-6 md:mb-0">
+            <a
+              href="#"
+              className="block p-4 rounded-xl ag-courses-item shadow hover:bg-blue-300 hover:text-black transition duration-300 relative ml-10"
+            >
+              <div className="h-32 mb-4 flex items-center justify-center">
+                <img src={Documento} alt="Estudiantes" className="h-full" />
+              </div>
+              <div className="text-xl font-bold">Documentos</div>
+              <div>Documentos de estudiantes y asesores</div>
+            </a>
+          </div>
         </div>
-        <div className="ag-courses-item_date-box" style={{textDecoration:"none"}}>
-          Proyectos del área de ingeniería
-        </div>
-      </a>
-    </div>
-    <div className="ag-courses_item shadow-drop-bl">
-      <a href="#" className="ag-courses-item_link">
-        <div className="ag-courses-item_bg" />
-        <div style={{ display: "flex" }}>
-          <img src={Asesores} alt="..." style={{ height: 100 }} />
-          <div className="ag-courses-item_title">Asesores</div>
-        </div>
-        <div className="ag-courses-item_date-box">
-          Asesores del área de ingeniería
-        </div>
-      </a>
-    </div>
-    <div className="ag-courses_item shadow-drop-bl">
-      <a href="#" className="ag-courses-item_link">
-        <div className="ag-courses-item_bg" />
-        <div style={{ display: "flex" }}>
-          <img src={Estudiante} alt="..." style={{ height: 100 }} />
-          <div className="ag-courses-item_title">Estudiantes</div>
-        </div>
-        <div className="ag-courses-item_date-box">
-          Estudiantes del área de ingeniería
-        </div>
-      </a>
-    </div>
-    <div className="ag-courses_item shadow-drop-bl">
-      <a href="#" className="ag-courses-item_link">
-        <div className="ag-courses-item_bg" />
-        <div style={{ display: "flex" }}>
-          <img src={Documento} alt="..." style={{ height: 100 }} />
-          <div className="ag-courses-item_title">Documentos</div>
-        </div>
-        <div className="ag-courses-item_date-box">
-          Documentos de estudiantes y asesores
-        </div>
-      </a>
-    </div>
-  </div>
-</div>
-
-        </>
-    )
+      </div>
+    </>
+  );
 }
-export default Modulo6
+
+export default Modulo6;
